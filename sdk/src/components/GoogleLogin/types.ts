118f80ea654dd32e038940913ff5a760fa775783
lichao <lichao@fun-chain.com>
2024-11-08 19:46:51 +0800

    delete sdk

--- sdk/src/components/GoogleLogin/types.ts
+++
@@ -1,7 +0,0 @@
-import { IUserInfo } from "@/components/ZkLoginProvider/types";
-
-export interface IGoogleLoginProps {
-  clientId: string;
-  loadingContent?: React.ReactNode | (() => React.ReactNode);
-  userContent: React.ReactNode | ((userInfo: IUserInfo) => React.ReactNode);
-}
